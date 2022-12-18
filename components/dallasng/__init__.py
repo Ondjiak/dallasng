91f3ea02096c467ce4c322b6d836170d990ed680
Ondjiak <73355015+Ondjiak@users.noreply.github.com>
2022-12-18 01:10:46 +0100

    Update __init__.py

--- components/dallasng/__init__.py
+++ components/dallasng/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
+from esphome import latest
 from esphome.const import CONF_ID, CONF_PIN
 
 MULTI_CONF = True
