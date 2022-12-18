8b6911469b19508f1e1768342a3a20262c3cff66
Ondjiak <73355015+Ondjiak@users.noreply.github.com>
2022-12-18 01:15:55 +0100

    Update __init__.py

--- components/dallasng/__init__.py
+++ components/dallasng/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome import latest
 from esphome.const import CONF_ID, CONF_PIN
 
 MULTI_CONF = True
@@ -21,7 +20,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add_library("https://github.com/pstolarz/OneWireNg", latest)
+    cg.add_library("https://github.com/pstolarz/OneWireNg", "*")
 
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_pin(pin))
